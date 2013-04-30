3baec9451b82b933ac3f920b3d4cb0100b515b60
Samuli Seppänen <samuli@openvpn.net>
2013-04-30 16:19:46 +0300

    Added ldapadmin::template define for managing custom template files
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,12 +8,21 @@ class ldapadmin::params {
     case $::osfamily {
         'RedHat': {
             $package_name = 'phpldapadmin'
+            $template_dir = '/usr/share/phpldapadmin/templates'
+            $template_group = 'root'
+            $template_mode = 644
         }
         'Debian': {
             $package_name = 'phpldapadmin'
+            $template_dir = '/etc/phpldapadmin/templates'
+            $template_group = 'www-data'
+            $template_mode = 640
         }
         default: {
             $package_name = 'phpldapadmin'
+            $template_dir = '/etc/phpldapadmin/templates'
+            $template_group = 'www-data'
+            $template_mode = 640
         }
     }
 }
---
+++ manifests/template.pp
@@ -0,0 +1,50 @@
+#
+# == Define: ldapadmin::template
+#
+# Install a phpldapadmin template from the Puppet fileserver. Template files are 
+# currently not included in the ldapadmin module as they may be highly 
+# site-specific.
+#
+# == Parameters
+#
+# [*filename*]
+#   Name of the file on Puppet files directory (typically /etc/puppet/files)
+# [*type*]
+#   Template type, either "creation" or "modification". Will determine which 
+#   template directory the file goes into.
+#
+# == Examples
+#
+# ldapadmin::template { 'custom inetOrgPerson creation template':
+#   filename => 'custom_create_inetOrgPerson.xml',
+#   type => 'creation',
+# }
+#
+# == Authors
+#
+# Samuli Seppänen <samuli@openvpn.net>
+#
+# == License
+#
+# BSD-lisence
+# See file LICENSE for details
+#
+define ldapadmin::template(
+    $filename,
+    $type
+)
+{
+
+    include ldapadmin::params
+
+    file { "ldapadmin-template-${type}-${filename}":
+        name => "$ldapadmin::params::template_dir/${type}/${filename}",
+        ensure => present,
+        source => "puppet:///files/${filename}",
+        owner => root,
+        group => $ldapadmin::params::template_group,
+        mode => $ldapadmin::params::template_mode,
+        require => Class['ldapadmin::install'],
+    }
+
+}
